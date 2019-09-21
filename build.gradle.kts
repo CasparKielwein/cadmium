387f6681aac22359f2626def4840008500c02696
CasparKielwein <Caspar@Kielwein.de>
2019-09-21 10:06:03 +0200

    made gradle more idiomatic

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     kotlin("jvm") version "1.3.50"
-    id ("org.jetbrains.dokka").version("0.9.18")
+    id("org.jetbrains.dokka").version("0.9.18")
+    maven
 }
 
+group = "com.github.CasparKielwein"
 version = "0.1"
 
 repositories {
@@ -25,9 +27,9 @@ dependencies {
     testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 
     //selenium plugin
-    compile("org.seleniumhq.selenium:selenium-firefox-driver:3.141.59")
-    compile("org.seleniumhq.selenium:selenium-server:3.141.59")
-    compile("org.seleniumhq.selenium:selenium-java:3.141.59")
+    implementation("org.seleniumhq.selenium:selenium-firefox-driver:3.141.59")
+    implementation("org.seleniumhq.selenium:selenium-server:3.141.59")
+    implementation("org.seleniumhq.selenium:selenium-java:3.141.59")
 }
 
 configure<JavaPluginConvention> {
