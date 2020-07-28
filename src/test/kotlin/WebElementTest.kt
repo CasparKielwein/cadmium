0f745606eb51754c1b6ac7abf91cee2d2b0b4366
Feiko Nanninga <feiko.nanninga@primekey.com>
2020-07-28 15:20:44 +0200

    Add test/example for nested XPath select

--- src/test/kotlin/WebElementTest.kt
+++ src/test/kotlin/WebElementTest.kt
@@ -10,13 +10,13 @@ internal class TestWebElement {
 
     @Test
     fun testEnterReplace() = headlessFirefox().browse(URL("https://en.wikipedia.org/wiki")) {
-        element(Id("searchInput")). enter("cheese")
+        element(Id("searchInput")).enter("cheese")
         assertEquals("cheese", element(Id("searchInput")).value)
 
-        element(Id("searchInput")). enter("cheese")
+        element(Id("searchInput")).enter("cheese")
         assertEquals("cheesecheese", element(Id("searchInput")).value)
 
-        element(Id("searchInput")). enter("cheesecake", replace = true)
+        element(Id("searchInput")).enter("cheesecake", replace = true)
         assertEquals("cheesecake", element(Id("searchInput")).value)
     }
 
@@ -29,7 +29,7 @@ internal class TestWebElement {
     }
 
     @Test
-    fun testElementsList()  = headlessFirefox().browse(URL("http://the-internet.herokuapp.com/drag_and_drop")) {
+    fun testElementsList() = headlessFirefox().browse(URL("http://the-internet.herokuapp.com/drag_and_drop")) {
         val columns = elements(ClassName("column"))
 
         assertEquals(2, columns.size)
@@ -45,5 +45,17 @@ internal class TestWebElement {
         assertEquals(2, columns.size)
         assertEquals("A", columns[0].text)
         assertEquals("B", columns[1].text)
+    }
+
+    @Test
+    fun testNestedXpath() = headlessFirefox().browse(URL("http://the-internet.herokuapp.com/challenging_dom")) {
+        // related: https://stackoverflow.com/questions/24661222/understanding-webelement-findelement-and-xpath
+        assertEquals("Iuvaret0", element(XPath("//td[1]")).text, "Incorrectly selected first cell (td)")
+
+        // For some reason, we cannot query `td[1]` directly `.//` has to be prepended.
+        assertEquals("Iuvaret1", element(XPath("//tr[2]")).element(XPath(".//td[1]")).text, "Incorrectly selected first cell (td) of second row (tr)")
+        element(XPath("//tr[2]")) {
+            assertEquals("Iuvaret1", element(XPath(".//td[1]")).text, "Incorrectly selected first cell (td) of second row (tr)")
+        }
     }
 }
